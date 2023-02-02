316dcc0fda74866bfac3e60296bc2092356e49eb
YASHURA <61384503+yanukadeneth99@users.noreply.github.com>
2023-02-02 23:34:58 +0530

    Updated Hardhat Config File

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,34 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-//* How to change this file
 /*
-- Uncomment the verfication lines if you want to verify your contract. Make sure you do the same in the `deploy.js` in the scripts folder.
-
-Make sure you have created a file called `.env` which has the following lines. Change the values into your URL and private key :
-
-ALCHEMY_API_KEY_URL="https://........"
-WALLET_PRIVATE_KEY="e17ubj82ybe......"
-
-
-# Make sure to add the follow line and change the value if you want to verify and remove the `#`
-# SCAN_KEY="ib813hbiad......"
-
-*/
+* Default Template for Reference 
 
 module.exports = {
   solidity: "0.8.9",
-  defaultNetwork: "goerli",
+  defaultNetwork: "mumbai",
   networks: {
     mumbai: {
       url: process.env.ALCHEMY_API_KEY_URL,
       accounts: [process.env.WALLET_PRIVATE_KEY],
     },
   },
-  //* Uncomment the follow lines for verfication process
-  // etherscan: {
-  //   apiKey: {
-  //     polygonMumbai: process.env.SCAN_KEY,
-  //   },
-  // },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.SCAN_KEY,
+    },
+  },
+};
+*/
+
+// Configuration
+/*
+  solidity - The version of solidity compiler
+  defaultNetwork - The Default network to run (Without running --network-name)
+  networks - Object which contains the network information
+  etherscan - Object to fill in EtherScan Information for contract verification
+*/
+module.exports = {
+  solidity: "0.8.9",
 };
